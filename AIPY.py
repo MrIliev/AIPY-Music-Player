f750d7cdeb9cbe5924bad267e9133179bc196d13
MrIliev <iliev.antonio@abv.bg>
2015-06-08 18:36:07 +0300

    Create AIPY.py

---
+++ AIPY.py
@@ -0,0 +1,46 @@
+import sys
+from PyQt4 import Qt
+from PyQt4.phonon import Phonon
+
+class AIPYbody():
+	# in the class body are all functionalities of the AIPY Music Player
+	# body will be one window from PyQT 
+	def __init__ (self):
+		pass
+
+	def Display():
+		# used for showing the remaining time, or elapsed time of song, it is posible to 
+		# have visulations
+		pass
+
+	def PlayList():
+		# defines play list with files selected by user
+		pass
+
+	def PlayButton():
+		#button to start the music file, if there is not you will be able to choose one
+		pass
+
+	def PauseButton():
+		# button to pause the music file, can be together with play
+		pass
+
+	def StopButton():
+		# button to stop the music file, and when you push play it starts from begining
+		pass
+
+	def NextButton():
+		# button for next song in playlist, if there is not starts same song again
+		pass
+
+	def PrevButton():
+		# button for previous song in playlist, if there is not starts the same song again
+		pass
+
+	def OnlineStreaming():
+		# button  which opens new window to select source for online streaming
+		pass
+
+	def EQ():
+		# button which opens window with equilaizer(optional)
+		pass
